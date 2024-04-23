a3dc5505f28ffdc041a7d07858b2c0e144c2bf35
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-04-23 16:42:33 +0300

    created store

---
+++ src/redux/store.js
@@ -0,0 +1,27 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistStore,
+} from 'redux-persist';
+import { filterReducer } from './filter/reducer';
+import { favoritesReducer } from './favorites/reducer';
+
+export const store = configureStore({
+  reducer: {
+    filter: filterReducer,
+    favorites: favoritesReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
