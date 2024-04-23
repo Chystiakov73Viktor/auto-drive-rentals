0e500f831eeec37f3134e382b9e95cf9909bf8d4
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-04-23 17:07:11 +0300

    add react-redux react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import ReactDOM from 'react-dom';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
