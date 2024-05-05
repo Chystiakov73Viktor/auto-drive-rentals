edc1dda7f9e631caadaf60eeb96c551476fd5617
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-05-05 22:24:47 +0300

    API

---
+++ src/services/db.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+axios.defaults.baseURL = `https://${API_KEY}.mockapi.io`;
+
+export const fetchGetCars = async () => {
+  const response = await axios.get('/adverts');
+  return response.data;
+};
+
+export const fetchAddCar = async (car) => {
+  const response = await axios.post('/adverts', car);
+  return response.data;
+};
+
+export const fetchDeleteCar = async id => {
+  const response = await axios.delete(`/adverts/${id}`);
+  return response.data;
+};
