4c7b022a3a0e1cbed27eb9cd58d3ae771e0513ae
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-05-05 22:39:15 +0300

    created component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import { Suspense } from "react";
+import { AppBar } from "components/AppBar/AppBar";
+import { Loader } from "components/Loader/Loader";
+
+export const Layout = () => {
+  return (
+    <>
+      <AppBar />
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </main>
+    </>
+  );
+};
