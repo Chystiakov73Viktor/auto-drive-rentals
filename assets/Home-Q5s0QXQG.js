79244a91d871811d5834d2d59fd33919644a7b18
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-05-05 19:40:01 +0000

    Deploying to gh-pages from @ Chystiakov73Viktor/auto-drive-rentals@4c7b022a3a0e1cbed27eb9cd58d3ae771e0513ae 🚀

---
+++ assets/Home-Q5s0QXQG.js
@@ -0,0 +1,49 @@
+import{u as o,j as t}from"./index-Dj6fRjdx.js";import{d as e,S as i}from"./autoCar-CPhA53uH.js";const a=o.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  background: linear-gradient(to bottom, #0d0c0b, #626161);
+
+  .containerFirstColor {
+    width: 100px;
+    height: 100px;
+    background: linear-gradient(to bottom, #d76432, #f74600);
+    clip-path: polygon(0% 0%, -75% 100%, 82% 75%, 100% -44%);
+  }
+
+  .containerSecondColor {
+    width: 100px;
+    height: 100px;
+    background: linear-gradient(to bottom, #dc470d, #cb8f77);
+    clip-path: polygon(-95% 87%, 45% 41%, 100% -100%, 100% 100%);
+  }
+
+  .containerImgColor {
+    flex: 1;
+    background: linear-gradient(to bottom, #dc470d, #cb8f77);
+    clip-path: polygon(8% 44%, 16% 100%, 100% 100%, 100% -16%);
+  }
+
+  .containerImg {
+    flex: 1;
+    clip-path: polygon(13% 47%, 20% 100%, 100% 100%, 100% -11%);
+    height: auto;
+    overflow: hidden;
+  }
+
+  .carImg {
+    width: 100%;
+    height: auto;
+  }
+
+  .titleFirst {
+    position: absolute;
+    top: 50%;
+    font-weight: 500;
+    font-size: 48px;
+    line-height: 117%;
+    letter-spacing: -0.02em;
+    color: #fff;
+  }
+`,r=()=>t.jsxs(a,{children:[t.jsx("div",{className:"containerFirstColor",children:t.jsx("div",{className:"containerSecondColor"})}),t.jsx("div",{className:"containerImgColor",children:t.jsx("div",{className:"containerImg",children:t.jsx("img",{className:"carImg",src:e,alt:"car"})})}),t.jsx("h1",{className:"titleFirst",children:"Looking for a vehicle? You’re at the right place."})]});function c(){return t.jsx(i,{className:"carsHomeSection",titleClassName:"titleCatalog",children:t.jsx(r,{})})}export{c as default};
---
+++ assets/autoCar-CPhA53uH.js
@@ -0,0 +1,18 @@
+import{u as e,j as n,C as r}from"./index-Dj6fRjdx.js";const i=e.section`
+  padding: 80px 0;
+  background: #f8f8f8;
+  &.hero {
+    padding: 0;
+    background: #fff;
+  }
+  &.carsHomeSection {
+    padding: 0;
+  }
+  & .titleCatalog {
+    font-family: "Manrope", sans-serif;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 1.5;
+    color: #121417;
+  }
+`,d=({className:a,titleText:s,titleClassName:o,children:t})=>n.jsx(i,{className:a,children:n.jsxs(r,{children:[s&&n.jsx("h2",{className:o,children:s}),t]})}),f="/assets/autoCar-tTE31l7z.jpg";export{d as S,f as d};
