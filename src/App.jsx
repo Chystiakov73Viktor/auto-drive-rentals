01bb82b0d67e5866f331a9e8b6e6de3c64e55c53
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-05-05 22:13:29 +0300

    lazy loading routing is used

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { lazy } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Layout } from "components/Layout/Layout";
+
+const Home = lazy(() => import("pages/Home"));
+const Catalog = lazy(() => import("pages/Catalog"));
+const Favorites = lazy(() => import("pages/Favorites"));
 
 function App() {
   return (
-    <>
-    
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 }
 
