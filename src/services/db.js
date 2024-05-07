c1220a47d67b45e0cb46f0c44d2a58baa1f496e8
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-05-07 21:29:22 +0300

    fix

--- src/services/db.js
+++ src/services/db.js
@@ -2,19 +2,19 @@ import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_API_TEST;
 
-axios.defaults.baseURL = `https://${API_KEY}.mockapi.io`;
+axios.defaults.baseURL = `https://${API_KEY}.mockapi.io/adverts`;
 
 export const fetchGetCars = async () => {
-  const response = await axios.get('/adverts');
+  const response = await axios.get('/');
   return response.data;
 };
 
 export const fetchAddCar = async (car) => {
-  const response = await axios.post('/adverts', car);
+  const response = await axios.post('/', car);
   return response.data;
 };
 
 export const fetchDeleteCar = async id => {
-  const response = await axios.delete(`/adverts/${id}`);
+  const response = await axios.delete(`/${id}`);
   return response.data;
 };
