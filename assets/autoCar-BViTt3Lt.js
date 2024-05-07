8f9be3b31eb8ff4e58064539b40751507dd4452c
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-05-07 18:49:40 +0000

    Deploying to gh-pages from @ Chystiakov73Viktor/auto-drive-rentals@eba1cff91fb7cfb2b3e78847a777b3ee8e5d38c8 🚀

--- assets/autoCar-BViTt3Lt.js
+++ assets/autoCar-BViTt3Lt.js
@@ -1,4 +1,4 @@
-import{u as e,j as a,C as r}from"./index-DTNClKTb.js";const i=e.section`
+import{u as e,j as a,C as r}from"./index-CmsbupQC.js";const i=e.section`
   padding: 80px 0;
   background: #f8f8f8;
   &.hero {
